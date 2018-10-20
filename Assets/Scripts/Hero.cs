b0fd94823b2554d0d13edf5ee2b39214f2e42d00
KazumaGun <KazumaGun@users.noreply.github.com>
2018-10-19 20:19:58 -0700

    Initial commit

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    //HERO MOVEMENTS\\
+    public float heroSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D heroRigidbody;
+
+    //CHECKING FOR GORUND\\
+    public bool isGrounded;
+    public LayerMask whatIsGround;
+
+
+	
+	void Start ()
+    {
+        heroRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update ()
+    {
+        heroRigidbody.velocity = new Vector2(heroSpeed, heroRigidbody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            heroRigidbody.velocity = new Vector2(heroRigidbody.velocity.x, jumpForce);
+        }
+	}
+}
