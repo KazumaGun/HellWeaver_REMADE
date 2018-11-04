8dd071fd5d35762c1ccb3e5921a58a06bba1f1a9
KazumaGun <KazumaGun@users.noreply.github.com>
2018-11-03 22:03:52 -0700

    Menus
    Created Menus, more scenes, and links to menus.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector2 velocity;
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject Hero;
+
+
+    //CAMERA BOUNDS\\
+    public bool cameraBounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Hero = GameObject.FindGameObjectWithTag("Hero");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float positionX = Mathf.SmoothDamp(transform.position.x , Hero.transform.position.x , ref velocity.x, smoothTimeX);
+        float positionY = Mathf.SmoothDamp(transform.position.y, Hero.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(positionX, positionY, transform.position.z);
+
+
+        //CAMERA BOUNDS\\
+        if (cameraBounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x) ,
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y) ,
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z) );
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseUI;
+    private bool isPaused = false;
+
+
+	void Start ()
+    {
+        PauseUI.SetActive(false);
+	}
+	
+	
+	void Update ()
+    {
+		if(Input.GetButtonDown("Pause"))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;         //setts toime to zero, time is nothing
+        }
+
+        if(!isPaused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;  //back to normal time can make slo motion
+        }
+	}
+
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void StartOver()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void RedoTutorial()
+    {
+        Application.LoadLevel(1);
+    }
+}
